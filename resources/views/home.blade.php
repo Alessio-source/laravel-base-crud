c2b7ae838d2f3044de5247d695fa558b427f8f69
Alessio Pancia <alessiop2801@gmail.com>
2021-02-10 14:49:37 +0100

    bootstrap

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,16 +5,22 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
         <link href = {{ asset("bootstrap/app.css") }} rel="stylesheet" />
 
         <title>Birre</title>
     </head>
     <body>
+
+        <main class="container">
         @foreach ($beer as $item)
             <div class="Birra">
                 <h2>{{ $item->name }}</h2>
                 <p>{{ $item->description }}</p>
             </div>
         @endforeach
+        </main>
+        
     </body>
 </html>
